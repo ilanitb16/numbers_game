919eb622dc7f2807b78df9e9c957d0988fbf7c10
ilanitb16 <ilanit.berdichevski@gmail.com>
2024-02-02 17:14:15 +0200

    code refactoring

--- test/game.s
+++ test/game.s
@@ -1,23 +1,25 @@
 .section .data                              # contains initialized data
-	counter: .long 5                        # amount of loops
-    user_number: .int 0                     # rReserve space for the input integer
+	counter: .int 5                        # amount of loops
+    user_number: .int 0                     # reserve space for the user number
     computer_number: .int 0
 
 .section .rodata	                        # read only data section
     str: .string "Guess a number between 0 and 9:\n"
-    computer_print_format: .string "My number is: %d\n"
+    computer_print_format: .string "Computer number is: %d\n"
     user_print_format: .string "User number is %d\n"
     time_print_format: .string "Time is: %d\n"
+    numbers_are_equal_message: .string "Numbers are equal\n"
+    numbers_are_not_equal_message: .string "Numbers are not equal\n"
     format: .string "%d"                    # format string for scanf, specifying integer input
     seed: .long 0
 
-.text	#the beginnig of the code
+.text	                                    # the beginnig of the code
 .globl	do_main	                            # the label "do_main" is used to state the initial point of this program
 .type	do_main, @function	                # the label "do_main" representing the beginning of a function
 
 .extern my_time
 
-do_main:	# the main function:
+do_main:	                                # the main function:
     pushq %rbp		                        # save the old frame pointer
     movq %rsp, %rbp	                        # create the new frame pointer
 
@@ -51,28 +53,49 @@ do_main:	# the main function:
 
         # print user number
         movq $user_print_format, %rdi       # pass format string to the function
-        movq [user_number], %rsi            # pass user number to the function
+        movq [user_number], %rsi            # pass user number to the function via %rsi
         movq $0, %rax                       # clear rax registry
         call printf                         # print user number
 
         # generate a random number
         call rand                           # Call the rand function, the result stored in %rax
 
-        # divide rax by 10 and get reminder in rdx to get a number between 0 and 9
-        movq $10, %rbx
+        # divide %rax by 10 to get a number between 0 and 9. this will put a reminder into rdx
+        movq $1, %rbx
         div %rbx
+
+        #movq %rdx, [computer_number]
+
+        cmp %rdx, [user_number]              # compare numbers
+        je numbers_are_equal                 # jump to 'numbers_are_equal' label
+        jmp numbers_are_not_equal            # jump to 'numbers_are_not equal' label
+
+    return_from_print_result:       # continue
 
         # print generated random number
         movq $computer_print_format, %rdi   # pass format string to the function
-        movq %rdx, %rsi                     # pass reminder to the function
+        #movq %rdx, %rsi                    # pass computer number (%rdx) to the function
         movq $0, %rax                       # clear rax registry
         call printf                         # print random value
 
         mov counter, %ecx                   # restore ecx value
-        loop   start_loop                   # jump to start if loop counter is not zero
+        loop start_loop                     # jump to start if loop counter is not zero
 
 
     movq	$0, %rax	                    # return value is zero (just like in c - we tell the OS that this program finished seccessfully)
     movq	%rbp, %rsp	                    # restore the old stack pointer - release all used memory.
     popq	%rbp		                    # restore old frame pointer (the caller function frame)
     ret			                            # return to caller function (OS)
+
+ numbers_are_equal:
+    movq $numbers_are_equal_message, %rdi   # pass format string to the function
+    movq $0, %rax                           # clear rax registry
+    call printf                             # print random value
+    jmp return_from_print_result
+
+
+ numbers_are_not_equal:
+     movq $numbers_are_not_equal_message, %rdi   # pass format string to the function
+     movq $0, %rax                           # clear rax registry
+     call printf                             # print random value
+     jmp return_from_print_result
